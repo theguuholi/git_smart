05bbbd25ed80e8bc4499be9179297983e25af63e
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-03-10 06:19:34 -0300

    repo added function

--- lib/git_smart_web/live/page_live/repo/repo.ex
+++ lib/git_smart_web/live/page_live/repo/repo.ex
@@ -1,3 +1,16 @@
 defmodule GitSmartWeb.PageLive.Repo do
   use GitSmartWeb, :live_component
+
+  def update(assigns, socket) do
+    socket =
+      socket
+      |> assign(assigns)
+      |> assign(is_added: false)
+
+    {:ok, socket}
+  end
+
+  def handle_event("add_repo", _, socket) do
+    {:noreply, assign(socket, is_added: true)}
+  end
 end
--- test/git_smart_web/live/page_live/repo_test.exs
+++ test/git_smart_web/live/page_live/repo_test.exs
@@ -12,6 +12,20 @@ defmodule GitSmartWeb.PageLive.RepoTest do
       assert has_element?(lv, "#{main_element}>div>div>span", "1,000")
       assert has_element?(lv, "#{main_element}>div>div>p", "pumpkin123")
       assert has_element?(lv, "#{main_element}>div>div>button")
+      refute has_element?(lv, "#{main_element}>div>div", "Repo added!")
+    end
+  end
+
+  describe "handle_event/3 - add_repo" do
+    test "add a repo", %{conn: conn} do
+      {:ok, lv, _html} = live(conn, ~p"/")
+
+      main_element = "li#repo-elixir"
+      refute has_element?(lv, "#{main_element}>div>div", "Repo added!")
+
+      lv |> element("button[phx-click=add_repo]") |> render_click()
+
+      assert has_element?(lv, "#{main_element}>div>div", "Repo added!")
     end
   end
 end
