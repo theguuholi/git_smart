8d75e40a8c1a4db01f23957159af5694114578da
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-03-11 15:16:52 -0300

    update the scroll

--- lib/git_smart_web/live/page_live/index.ex
+++ lib/git_smart_web/live/page_live/index.ex
@@ -4,23 +4,36 @@ defmodule GitSmartWeb.PageLive.Index do
   alias GitSmart.Repositories
 
   def handle_params(params, _, socket) do
-    socket = assign_repositories(socket, params)
-    {:noreply, socket}
+    {:noreply, assign_repositories(socket, params)}
   end
 
   def handle_event("load_more", _, socket) do
+    language = socket.assigns.language
+    page = socket.assigns.page
+    repositories = Repositories.list(language, page)
+
+    socket =
+      socket
+      |> update(:page, &(&1 + 1))
+      |> update_socket(repositories)
+
     {:noreply, socket}
   end
 
   defp assign_repositories(socket, params) do
     language = params["language"] || "elixir"
-    page = params["page"] || "1"
+    page = 1
+    socket = assign(socket, language: language, page: page)
     repositories = Repositories.list(language, page)
+    update_socket(socket, repositories)
+  end
 
+  defp update_socket(socket, repositories) do
     case repositories do
       {:error, message} ->
         socket
         |> put_flash(:error, message)
+        |> stream(:repositories, [], at: 0)
 
       repositories ->
         stream(socket, :repositories, repositories, at: 0)
