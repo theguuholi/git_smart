84ece0f829e19fb5ae700311aa3b600c381b7c61
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-03-16 06:36:05 -0300

    Update links and add repository details to show page

--- lib/git_smart_web/live/repository_live/show/show.ex
+++ lib/git_smart_web/live/repository_live/show/show.ex
@@ -1,4 +1,10 @@
 defmodule GitSmartWeb.RepositoryLive.Show do
   use GitSmartWeb, :live_view
   alias GitSmart.Repositories
+
+  def mount(%{"git_id" => git_id}, _, socket) do
+    repository = Repositories.get_repository_by_git_id(git_id)
+    IO.inspect(repository)
+    {:ok, assign(socket, repository: repository)}
+  end
 end
