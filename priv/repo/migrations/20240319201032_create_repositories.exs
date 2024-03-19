88cba9ee251743d49ee2b87dc4db2b3965cd2830
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-03-19 17:17:15 -0300

    create migrations

---
+++ priv/repo/migrations/20240319201032_create_repositories.exs
@@ -0,0 +1,22 @@
+defmodule GitSmart.Repo.Migrations.CreateRepositories do
+  use Ecto.Migration
+
+  def change do
+    create table(:repositories, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :avatar_url, :string
+      add :description, :text
+      add :forks, :integer
+      add :git_id, :integer
+      add :language, :string
+      add :full_name, :string
+      add :open_issues, :integer
+      add :html_url, :string
+      add :watchers_count, :integer
+
+      timestamps()
+    end
+
+    create unique_index(:repositories, [:git_id])
+  end
+end
