84d47737cf3b53bacaded08f15b212993bf88698
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-03-19 12:34:26 -0300

    finish all tests

--- lib/git_smart_web/live/repository_live/show/show.ex
+++ lib/git_smart_web/live/repository_live/show/show.ex
@@ -4,7 +4,6 @@ defmodule GitSmartWeb.RepositoryLive.Show do
 
   def mount(%{"git_id" => git_id}, _, socket) do
     repository = Repositories.get_repository_by_git_id(git_id)
-    IO.inspect(repository)
     {:ok, assign(socket, repository: repository)}
   end
 end
