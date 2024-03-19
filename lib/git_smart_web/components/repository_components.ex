56faa95ea25b308965a4652df48735751287923a
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-03-19 17:39:50 -0300

    reuse repo component

---
+++ lib/git_smart_web/components/repository_components.ex
@@ -0,0 +1,30 @@
+defmodule GitSmartWeb.RepositoryComponents do
+  use Phoenix.Component
+
+  slot :action
+  slot :details
+
+  def repository_item(assigns) do
+    ~H"""
+    <a href="" class="flex space-x-3 p-2 bg-sky-50 hover:brightness-95 transition-all rounded-md">
+      <img src={@repository.avatar_url} class="h-6 w-6 rounded-full" />
+      <div class="flex-1 space-y-4">
+        <div class="flex items-center justify-between">
+          <h3><%= @repository.full_name %></h3>
+          <span>
+            <strong>Watchers:</strong> <%= @repository.watchers_count %>
+          </span>
+        </div>
+
+        <div class="flex items-center justify-between">
+          <p class="text-xs text-zinc-400">
+            <%= @repository.description %>
+          </p>
+          <%= render_slot(@action) %>
+        </div>
+        <%= render_slot(@details) %>
+      </div>
+    </a>
+    """
+  end
+end
